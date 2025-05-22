e15beea40a776b6ba4150e7fd60e602d67162cbd
Park Do Yeong <jhpark970602@naver.com>
2025-05-22 17:32:53 +0900

    1

--- backend/community/views.py
+++ backend/community/views.py
@@ -1,61 +1,42 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, permissions
+from .models import Thread, Comment, Like, ColorHistory
+from .serializers import ThreadSerializer, CommentSerializer, LikeSerializer, ColorHistorySerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import Thread, Comment
-from .serializers import ThreadSerializer, CommentSerializer
+from django.db.models import Sum
 
 class ThreadViewSet(viewsets.ModelViewSet):
     queryset = Thread.objects.all()
     serializer_class = ThreadSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    
-    def get_queryset(self):
-        queryset = Thread.objects.all()
-        book_id = self.request.query_params.get('book', None)
-        thread_type = self.request.query_params.get('type', None)
-        
-        if book_id:
-            queryset = queryset.filter(book_id=book_id)
-        if thread_type:
-            queryset = queryset.filter(thread_type=thread_type)
-            
-        return queryset.order_by('-created_at')
-    
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-    
-    @action(detail=True, methods=['post'])
-    def like(self, request, pk=None):
-        thread = self.get_object()
-        user = request.user
-        
-        if thread.likes.filter(id=user.id).exists():
-            thread.likes.remove(user)
-            return Response({'status': 'unliked'})
-        else:
-            thread.likes.add(user)
-            return Response({'status': 'liked'})
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    
-    def get_queryset(self):
-        return Comment.objects.filter(parent=None)  # 최상위 댓글만 반환
-    
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-    
-    @action(detail=True, methods=['post'])
-    def like(self, request, pk=None):
-        comment = self.get_object()
-        user = request.user
-        
-        if comment.likes.filter(id=user.id).exists():
-            comment.likes.remove(user)
-            return Response({'status': 'unliked'})
-        else:
-            comment.likes.add(user)
-            return Response({'status': 'liked'})
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ColorHistoryViewSet(viewsets.ModelViewSet):
+    queryset = ColorHistory.objects.all()
+    serializer_class = ColorHistorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @action(detail=True, methods=['get'])
+    def represent_color(self, request, pk=None):
+        user = self.get_object()
+        color_history = ColorHistory.objects.filter(user=user)
+        total_count = color_history.aggregate(Sum('count'))['count__sum'] or 0
+
+        if total_count == 0:
+            return Response({"represent_color": None})
+
+        color_weights = {}
+        for entry in color_history:
+            color_weights[entry.genre.color] = color_weights.get(entry.genre.color, 0) + entry.count
+
+        represent_color = max(color_weights, key=color_weights.get)
+        return Response({"represent_color": represent_color})
